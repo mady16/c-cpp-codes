f05d65735dc399aace9e8cba0d41ac25e5da1ac0
simonmrog <simonmrog@gmail.com>
2019-01-09 00:01:05 -0500

    linked lists using oop

--- linked-lists.cpp
+++ linked-lists.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 //each of the nodes of our list is a class
@@ -25,11 +26,11 @@ template <class T> class list {
 
     private:
 
-        node <T> *head, *p, *last, *previous;
+        node <T> *head, *p, *last;
 
     public:
 
-        list(): head(NULL), p(NULL), last(NULL), previous(NULL) {}
+        list(): head(NULL), p(NULL), last(NULL) {}
 
         ~list () {
 
@@ -44,7 +45,29 @@ template <class T> class list {
             cout << "list destroyed." << endl;
         }
 
-        //inserts a node at the begining of the list
+        bool is_empty () {
+
+            return (head == NULL);
+        }
+
+        //inserts a node at the beginning of the list
+        void append (T x) {
+
+            p = new node <T> (x);
+
+            if (head == NULL) {
+
+                head = p;
+                last = head;
+            }
+            else {
+
+                p->next = head;
+                head = p;
+            }
+        }
+
+        //inserts a node at the end of the list
         void push (T x) {
 
             p = new node <T> (x);
@@ -59,13 +82,65 @@ template <class T> class list {
                 last->next = p;
                 last = p;
             }
+        }
 
+        void pop () {
+
+            if (is_empty())
+                cout << "empty list.";
+            else if (head == last)
+                head = last = NULL;
+            else {
+
+                for (p = head; p->next != last; p = p->next);
+                delete (last);
+                last = p;
+                last->next = NULL;
+            }
+        }
+
+        void delete_at (int i) {
+
+            if (is_empty())
+                cout << "empty list.";
+            else if (i < size()) {
+
+                p = head;
+
+                if (i == 0) {
+
+                    head = p->next;
+                    delete (p);
+                }
+                else {
+
+                    for (int j=0; j < i-1; j++)
+                        p = p->next;
+
+                    node <T> *tmp = p->next;
+                    if (tmp->next != NULL) {
+
+                        p->next = tmp->next;
+
+                    }
+                    else {
+                        last = p;
+                        last->next = NULL;
+                    }
+
+                    delete (tmp);
+                }
+            }
+            else
+                cout << "Invalid index." << endl;
         }
 
         void print () {
 
             for (p = head; p != NULL; p = p->next)
-                cout << p.get_data();
+                cout << p->get_data() << " ";
+
+            cout << endl;
         }
 
         int size () {
@@ -85,15 +160,28 @@ template <class T> class list {
 
 int main () {
 
+    //initializing random seed
+    srand (time(NULL));
+
     list <int> l;
-    l.push(350);
-    l.push(5);
-    l.push(19);
-    l.push(4);
 
-    cout << l[2] << endl;
+    cout << "Is the list empty? " << l.is_empty() << endl;
+    l.push(rand() % 100);
+    l.push(rand() % 100);
+    l.push(rand() % 100);
+    l.append(rand() % 100);
+    l.push (rand() % 100);
+    cout << "Is the list still empty? " << l.is_empty() << endl;
+    cout << "l[2] = " << l[2] << endl << endl;
     cout << "size of the list: " << l.size() << endl;
+    l.print ();
 
+    l.pop ();
+    l.print();
+
+    l.delete_at (0);
+    l.print ();
+    l.delete_at (10);
 
     return (0);
 }
